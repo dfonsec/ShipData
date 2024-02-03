3e176a13abf8aa08e34bc7cc9c87d449fb2dff52
dfonsec <dfonseca@hmc.edu>
2024-02-02 23:31:37 -0800

    Implemented deleteDuplicateRows

---
+++ dataDeduplicate.py
@@ -0,0 +1,39 @@
+
+import openpyxl
+from openpyxl import load_workbook
+
+wb = openpyxl.load_workbook("/Users/danielfonseca/repos/ShipData/data/ship_maintence.xlsx")
+
+def deleteDuplicateRows(workbook):
+    ws=workbook['Ship_Maintenance_Cleaned']
+    rows = ws.iter_rows(min_row=1)
+    seenIDS = set()
+    resultSheet = workbook.create_sheet('No Duplicates')
+    
+    for row in rows:
+        if row[2].value not in seenIDS:
+            seenIDS.add(row[2].value)
+            row_with_values = [cell.value for cell in row]
+            resultSheet.append(row_with_values)
+    
+    return workbook.save("/Users/danielfonseca/repos/ShipData/data/resultData.xlsx")
+
+
+    
+
+
+            
+            
+    
+
+
+
+
+
+    
+    
+    
+    
+
+
+
