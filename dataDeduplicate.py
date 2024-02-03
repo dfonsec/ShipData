38bdfbf2095c0e315731f3a25a5b0446842c2985
dfonsec <dfonseca@hmc.edu>
2024-02-03 00:09:52 -0800

    Added function comments

--- dataDeduplicate.py
+++ dataDeduplicate.py
@@ -5,6 +5,18 @@ from openpyxl import load_workbook
 wb = openpyxl.load_workbook("/Users/danielfonseca/repos/ShipData/data/ship_maintence.xlsx")
 
 def deleteDuplicateRows(workbook):
+    """
+    Deletes duplicate rows based on unique Service Order IDs in the 'Ship_Maintenance_Cleaned' sheet.
+    Creates a new sheet named 'No Duplicates' and appends rows with distinct Service Order IDs.
+
+    Args:
+    - workbook: An openpyxl Workbook object containing the data.
+
+    Returns:
+    - The modified workbook with the 'No Duplicates' sheet.
+    - Saves the result to a new Excel file ('resultData.xlsx').
+    """
+    
     ws=workbook['Ship_Maintenance_Cleaned']
     rows = ws.iter_rows(min_row=1)
     seenIDS = set()
@@ -19,6 +31,15 @@ def deleteDuplicateRows(workbook):
     return workbook.save("/Users/danielfonseca/repos/ShipData/data/resultData.xlsx")
 
 def countUniqueRows(worksheet):
+    """
+    Counts the number of rows with unique Service Order IDs in the provided worksheet.
+
+    Args:
+    - worksheet: An openpyxl Worksheet object containing the data.
+
+    Returns:
+    - The count of rows with unique Service Order IDs.
+    """
     rows = worksheet.iter_rows(min_row=2)
     seenIDS = set()
 
